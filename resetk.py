551b754a7ff8bdb4a44e6cd9fe842cf33e8c304f
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-30 13:32:44 +0530

    Update resetk.py

--- resetk.py
+++ resetk.py
@@ -2,6 +2,9 @@ print("hello world")
 import requests
 
 url = ["https://strmltpym1.streamlit.app/"]
-for k in url:
-  response = requests.get(url[k])
-print(response.content)
+k = 0
+
+while k < len(url): 
+  response = requests.get(url[K])
+  k+=1
+  print(response.content)
