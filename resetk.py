2f19e2c71b21ccd2a084db9b5d9ee994a8db1467
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-30 13:29:08 +0530

    Update resetk.py

--- resetk.py
+++ resetk.py
@@ -1,1 +1,8 @@
 print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+url = ["https://strmltpym1.streamlit.app/"]
+for k in url:
+  response = requests.get(url[k])
+print(response.content)
